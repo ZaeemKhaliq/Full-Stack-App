42a2b739f16810a91d124c801b28e27c1736f847
Muhammad Zaeem Khaliq <57555591+ZaeemKhaliq@users.noreply.github.com>
2021-08-08 13:57:30 +0500

    Changed

--- http-common.js
+++ http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://full-stack-app-menn.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
